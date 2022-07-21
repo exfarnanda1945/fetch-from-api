4825a82f2a7f450bcd537b77bb5333d1e9e79cb1
exfarnanda1945 <exfarnanda1945@gmail.com>
2022-07-21 14:05:12 +0700

    add: main activity logic

--- app/src/main/java/com/example/fetchfromapi/MainActivity.kt
+++ app/src/main/java/com/example/fetchfromapi/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.fetchfromapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fetchfromapi.databinding.ActivityMainBinding
+import com.example.fetchfromapi.models.ProductResponseModel
+import com.example.fetchfromapi.network.Api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var rvProducts: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        rvProducts = binding.rvProducts
+
+        rvProducts.setHasFixedSize(true)
+
+        Api.retrofitService.list().enqueue(object : Callback<ProductResponseModel> {
+            override fun onResponse(
+                call: Call<ProductResponseModel>,
+                response: Response<ProductResponseModel>
+            ) {
+                if (response.isSuccessful) {
+                    val result: ProductResponseModel? = response.body()
+                    rvProducts.apply {
+                        adapter = result?.products?.let { ProductListAdapter(it) }
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ProductResponseModel>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
     }
 }
